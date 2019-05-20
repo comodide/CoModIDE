f7a642376d02a55b815462fcdead39f4d448f10a
Karl Hammar <karl@karlhammar.com>
2019-05-20 14:38:38 +0200

    Module annotation placement preferences implemented

--- src/main/java/com/comodide/views/ConfigurationView.java
+++ src/main/java/com/comodide/views/ConfigurationView.java
@@ -20,6 +20,12 @@ import org.slf4j.LoggerFactory;
 import com.comodide.patterns.PatternInstantiationConfiguration;
 import com.comodide.patterns.PatternInstantiationConfiguration.EdgeCreationAxiom;
 
+/**
+ * CoModIDE configuration view. Provides an interface through which users may select how CoModIDE should instantiate ontology design patterns
+ * into their target ontology.
+ * @author Karl Hammar <karl@karlhammar.com>
+ *
+ */
 public class ConfigurationView extends AbstractOWLViewComponent {
 
 	private static final long serialVersionUID = -9035122006146275611L;
@@ -27,12 +33,21 @@ public class ConfigurationView extends AbstractOWLViewComponent {
 	
 	private static final String USE_TARGET_NAMESPACE_ACTION = "useTarget";
 	private static final String KEEP_PATTERN_NAMESPACE_ACTION = "keepPattern";
+	private static final String METADATA_EXTERNAL_ACTION = "metadataExternal";
+	private static final String METADATA_INTERNAL_ACTION = "metadataInternal";
 	
 	private static final ActionListener namespaceButtonsListener = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			Boolean useTarget = (e.getActionCommand().equals(USE_TARGET_NAMESPACE_ACTION));
 			PatternInstantiationConfiguration.setUseTargetNamespace(useTarget);
+		}
+	};
+	private static final ActionListener metadataPlacementListener = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Boolean metadataExternal = (e.getActionCommand().equals(METADATA_EXTERNAL_ACTION));
+			PatternInstantiationConfiguration.setModuleMetadataExternal(metadataExternal);
 		}
 	};
 
@@ -46,22 +61,40 @@ public class ConfigurationView extends AbstractOWLViewComponent {
 		this.add(entityNamingLabel);
 		
 		Boolean useTargetNamespace = PatternInstantiationConfiguration.getUseTargetNamespace();
-		ButtonGroup group = new ButtonGroup();
+		ButtonGroup namespaceGroup = new ButtonGroup();
 		JRadioButton useTargetNamespaceButton = new JRadioButton("Use target namespace");
 		useTargetNamespaceButton.setActionCommand(USE_TARGET_NAMESPACE_ACTION);
 		useTargetNamespaceButton.addActionListener(namespaceButtonsListener);
-		group.add(useTargetNamespaceButton);
+		namespaceGroup.add(useTargetNamespaceButton);
 		useTargetNamespaceButton.setSelected(useTargetNamespace);
 		this.add(useTargetNamespaceButton);
 		
 		JRadioButton keepPatternNamespaceButton = new JRadioButton("Keep pattern namespace");
 		keepPatternNamespaceButton.setActionCommand(KEEP_PATTERN_NAMESPACE_ACTION);
 		keepPatternNamespaceButton.addActionListener(namespaceButtonsListener);
-		group.add(keepPatternNamespaceButton);
+		namespaceGroup.add(keepPatternNamespaceButton);
 		keepPatternNamespaceButton.setSelected(!useTargetNamespaceButton.isSelected());
 		this.add(keepPatternNamespaceButton);
 		
-		// TODO: Add additional configuration options
+		JLabel moduleMetadataLabel = new JLabel("Module annotations placement:");
+		moduleMetadataLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+		this.add(moduleMetadataLabel);
+		
+		Boolean moduleMetadataExternal = PatternInstantiationConfiguration.getModuleMetadataExternal();
+		ButtonGroup metadataGroup = new ButtonGroup();
+		JRadioButton metadataExternalButton = new JRadioButton("External (in importing parent ontology)");
+		metadataExternalButton.setActionCommand(METADATA_EXTERNAL_ACTION);
+		metadataExternalButton.addActionListener(metadataPlacementListener);
+		metadataGroup.add(metadataExternalButton);
+		metadataExternalButton.setSelected(moduleMetadataExternal);
+		this.add(metadataExternalButton);
+		
+		JRadioButton metadataInternalButton = new JRadioButton("Internal (in target ontology)");
+		metadataInternalButton.setActionCommand(METADATA_INTERNAL_ACTION);
+		metadataInternalButton.addActionListener(metadataPlacementListener);
+		metadataGroup.add(metadataInternalButton);
+		metadataInternalButton.setSelected(!metadataExternalButton.isSelected());
+		this.add(metadataInternalButton);
 		
 		JLabel edgeCreationAxiomsLabel = new JLabel("Edge creation axioms:");
 		edgeCreationAxiomsLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
