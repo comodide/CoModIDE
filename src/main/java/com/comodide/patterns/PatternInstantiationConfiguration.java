73718c8cde86cb960fc2bbaac616041ffe9a02df
Karl Hammar <karl@karlhammar.com>
2019-05-20 14:06:35 +0200

    Added configuration options for entity naming

--- src/main/java/com/comodide/patterns/PatternInstantiationConfiguration.java
+++ src/main/java/com/comodide/patterns/PatternInstantiationConfiguration.java
@@ -24,14 +24,17 @@ public class PatternInstantiationConfiguration {
 	    }
 	}
 	
+	private static final String PREFERENCES_SET_ID = "com.comodide.comodide";
+	private static final String INSTANTIATION_PREFERENCES_KEY = "instantiation_configuration";
+	private static final String EDGE_CREATION_AXIOMS_KEY = "edge_creation_axioms";
+	private static final String USE_TARGET_NAMESPACE_KEY = "use_target_namespace";
+	
 	private static final PreferencesManager PREFERENCES_MANAGER = PreferencesManager.getInstance();
-	private static final String PREFERENCES_SET_ID = "com.comodide.CoModIDE";
-	private static final String EDGE_CREATION_AXIOMS_KEY = "EDGE_CREATION_AXIOMS";
+	private static final Preferences PREFERENCES = PREFERENCES_MANAGER.getPreferencesForSet(PREFERENCES_SET_ID, INSTANTIATION_PREFERENCES_KEY);
 	
 	public static List<EdgeCreationAxiom> getSelectedEdgeCreationAxioms() {
-		Preferences prefs = PREFERENCES_MANAGER.getPreferencesForSet(PREFERENCES_SET_ID, EDGE_CREATION_AXIOMS_KEY);
 		List<EdgeCreationAxiom> retVal = new ArrayList<EdgeCreationAxiom>();
-		for (String s: prefs.getStringList(EDGE_CREATION_AXIOMS_KEY, new ArrayList<String>())) {
+		for (String s: PREFERENCES.getStringList(EDGE_CREATION_AXIOMS_KEY, new ArrayList<String>())) {
 			retVal.add(EdgeCreationAxiom.valueOf(s));
 		}
 		return retVal;
@@ -54,11 +57,18 @@ public class PatternInstantiationConfiguration {
 	}
 	
 	private static void setSelectedEdgeCreationAxioms(List<EdgeCreationAxiom> creationAxioms) {
-		Preferences prefs = PREFERENCES_MANAGER.getPreferencesForSet(PREFERENCES_SET_ID, EDGE_CREATION_AXIOMS_KEY);
 		List<String> listToStore = new ArrayList<String>();
 		for (EdgeCreationAxiom e: creationAxioms) {
 			listToStore.add(e.name());
 		}
-		prefs.putStringList(EDGE_CREATION_AXIOMS_KEY, listToStore);
+		PREFERENCES.putStringList(EDGE_CREATION_AXIOMS_KEY, listToStore);
+	}
+	
+	public static Boolean getUseTargetNamespace() {
+		return PREFERENCES.getBoolean(USE_TARGET_NAMESPACE_KEY, true);
+	}
+	
+	public static void setUseTargetNamespace(Boolean value) {
+		PREFERENCES.putBoolean(USE_TARGET_NAMESPACE_KEY, value);
 	}
 }
--- src/main/java/com/comodide/views/ConfigurationView.java
+++ src/main/java/com/comodide/views/ConfigurationView.java
@@ -1,13 +1,17 @@
 package com.comodide.views;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.List;
 
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 import org.slf4j.Logger;
@@ -20,15 +24,46 @@ public class ConfigurationView extends AbstractOWLViewComponent {
 
 	private static final long serialVersionUID = -9035122006146275611L;
 	private static final Logger log = LoggerFactory.getLogger(ConfigurationView.class);
+	
+	private static final String USE_TARGET_NAMESPACE_ACTION = "useTarget";
+	private static final String KEEP_PATTERN_NAMESPACE_ACTION = "keepPattern";
+	
+	private static final ActionListener namespaceButtonsListener = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Boolean useTarget = (e.getActionCommand().equals(USE_TARGET_NAMESPACE_ACTION));
+			PatternInstantiationConfiguration.setUseTargetNamespace(useTarget);
+		}
+	};
 
 	@Override
 	protected void initialiseOWLView() throws Exception {
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
+		JLabel entityNamingLabel = new JLabel("Entity naming:");
+		Font f = entityNamingLabel.getFont();
+		entityNamingLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+		this.add(entityNamingLabel);
+		
+		Boolean useTargetNamespace = PatternInstantiationConfiguration.getUseTargetNamespace();
+		ButtonGroup group = new ButtonGroup();
+		JRadioButton useTargetNamespaceButton = new JRadioButton("Use target namespace");
+		useTargetNamespaceButton.setActionCommand(USE_TARGET_NAMESPACE_ACTION);
+		useTargetNamespaceButton.addActionListener(namespaceButtonsListener);
+		group.add(useTargetNamespaceButton);
+		useTargetNamespaceButton.setSelected(useTargetNamespace);
+		this.add(useTargetNamespaceButton);
+		
+		JRadioButton keepPatternNamespaceButton = new JRadioButton("Keep pattern namespace");
+		keepPatternNamespaceButton.setActionCommand(KEEP_PATTERN_NAMESPACE_ACTION);
+		keepPatternNamespaceButton.addActionListener(namespaceButtonsListener);
+		group.add(keepPatternNamespaceButton);
+		keepPatternNamespaceButton.setSelected(!useTargetNamespaceButton.isSelected());
+		this.add(keepPatternNamespaceButton);
+		
 		// TODO: Add additional configuration options
 		
 		JLabel edgeCreationAxiomsLabel = new JLabel("Edge creation axioms:");
-		Font f = edgeCreationAxiomsLabel.getFont();
 		edgeCreationAxiomsLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
 		this.add(edgeCreationAxiomsLabel);
 		
