f13dd6da09b47b06ba48face289f0a413226717c
Cogan Shimizu <shimizu.5@wright.edu>
2019-05-21 09:53:45 -0400

    refactored labelchange handling

--- src/main/java/com/comodide/rendering/editor/SchemaDiagram.java
+++ src/main/java/com/comodide/rendering/editor/SchemaDiagram.java
@@ -1,23 +1,13 @@
 package com.comodide.rendering.editor;
 
 import java.text.NumberFormat;
-import java.util.Iterator;
-import java.util.List;
 
 import org.protege.editor.owl.model.OWLModelManager;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLDatatype;
-import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.comodide.axiomatization.AxiomManager;
-import com.comodide.rendering.sdont.model.SDEdge;
-import com.comodide.rendering.sdont.model.SDNode;
 import com.mxgraph.model.mxCell;
-import com.mxgraph.model.mxGeometry;
-import com.mxgraph.util.mxPoint;
-import com.mxgraph.view.mxCellState;
 import com.mxgraph.view.mxGraph;
 
 /**
@@ -37,11 +27,8 @@ public class SchemaDiagram extends mxGraph
 	 */
 	protected Object edgeTemplate;
 
-	/**
-	 * Manages the axioms to be added or removed based on the nodes and connections
-	 * inside of the graph
-	 */
-	private AxiomManager axiomManager;
+	/** Used for handling the changes to cell lables. i.e. add/remove axioms */
+	private LabelChangeHandler labelChangeHandler;
 
 	/**
 	 * Custom graph that defines the alternate edge style to be used when the middle
@@ -50,113 +37,26 @@ public class SchemaDiagram extends mxGraph
 	public SchemaDiagram(OWLModelManager modelManager)
 	{
 		setAlternateEdgeStyle("edgeStyle=mxEdgeStyle.ElbowConnector;elbow=vertical");
-		this.axiomManager = AxiomManager.getInstance(modelManager);
+		this.labelChangeHandler = new LabelChangeHandler(modelManager);
 		this.allowDanglingEdges = false;
 	}
 
 	@Override
-	public void cellLabelChanged(Object changedCell, Object newValue, boolean autoSize)
+	public void cellLabelChanged(Object cell, Object newValue, boolean autoSize)
 	{
 		model.beginUpdate();
 
 		try
 		{
 			log.info("[CoModIDE:SchemaDiagram] cellLabelChanged intercepted.");
-
-			// Cast the object into a cell
-			mxCell cell = (mxCell) changedCell;
-			// Get value of the object that is changing.
-			Object currentVal = cell.getValue();
-			// Typecast the received newValue to String.
-			String newLabel = (String) newValue;
-			// If v is a string, then that means it is a truly new node
-			if (currentVal instanceof String && cell.getStyle().equals(SDConstants.classShape)) // New class
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] New class detected.");
-				// Add the new class to the ontology
-				OWLClass clazz = this.axiomManager.addNewClass(newLabel);
-				// Create an SDNode wrapper for the Axiom
-				SDNode node = new SDNode(newLabel, false, (OWLEntity) clazz);
-				// Set the value in the model.
-				model.setValue(changedCell, node);
-			}
-			else if (currentVal instanceof String && cell.getStyle().equals(SDConstants.datatypeShape))
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] New datatype detected.");
-
-				// Add the new class to the ontology
-				List<OWLDatatype> datatypes = this.axiomManager.addDatatype(newLabel);
-				// there should just be one...
-				if (datatypes.size() == 1)
-				{
-					// Get the datatype
-					OWLDatatype datatype = datatypes.get(0);
-					// Create an SDNode wrapper for the Axiom
-					SDNode node = new SDNode(newLabel, true, (OWLEntity) datatype);
-					// Set the value in the model.
-					model.setValue(changedCell, node);
-				}
-				else
-				{
-					// Don't do anything.
-					// This will force them to type something as the label again
-					// TODO Eventually we want a descriptive message
-				}
-			}
-			else if (currentVal instanceof String && cell.isEdge())
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] new edge detected.");
-			}
-			else if (currentVal instanceof SDNode)
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] Change class detected.");
-				// Get current class
-				SDNode node = (SDNode) currentVal;
-
-				if (node.isDatatype())
-				{
-					// Add the new class to the ontology
-					List<OWLDatatype> datatypes = this.axiomManager.addDatatype(newLabel);
-					// there should just be one...
-					if (datatypes.size() == 1)
-					{
-						// Get the datatype
-						OWLDatatype datatype = datatypes.get(0);
-						// Create an SDNode wrapper for the Axiom
-						SDNode datanode = new SDNode(newLabel, true, (OWLEntity) datatype);
-						// Set the value in the model.
-						model.setValue(changedCell, datanode);
-					}
-					else
-					{
-						// Don't do anything.
-						// This will force them to type something as the label again
-						// TODO Eventually we want a descriptive message
-					}
-				}
-				else
-				{
-					OWLClass oldClass = (OWLClass) node.getOwlEntity();
-					// Change the class in the ontology
-					OWLEntity owlEntity = this.axiomManager.replaceClass(oldClass, newLabel);
-					node.update(newLabel, owlEntity);
-					model.setValue(changedCell, node);
-				}
-			}
-			else if (currentVal instanceof SDEdge)
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] Change property detected.");
-			}
-			else if (cell.isEdge()) // Enter branch if the label change is for a cell
-			{
-				log.info("\t[CoModIDE:SchemaDiagram] New Property detected.");
-			}
-			else
-			{
-				// TODO
-			}
-
-			// End Adding //
+			/* Process object into useful formats. */
+			mxCell changedCell = (mxCell) cell;
+			String newLabel    = (String) newValue;
+			// Handle the label change
+			newValue = labelChangeHandler.handle(changedCell, newLabel);
+			// Set the new value
+			model.setValue(cell, newValue);
+			// Autosize, if necessary.
 			if (autoSize)
 			{
 				cellSizeUpdated(changedCell, false);
@@ -166,6 +66,12 @@ public class SchemaDiagram extends mxGraph
 		{
 			model.endUpdate();
 		}
+	}
+
+	public void updateSchemaDiagramFromOntology(OWLOntologyChange change)
+	{
+		log.info("\t\t[CoModIDE:SchemaDiagram] Cascading Ontology Change.");
+		// TODO update the schema diagram here.
 	}
 
 	/** Sets the edge template to be used to inserting edges. */
