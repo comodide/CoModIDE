e1b2e1c82d17a19f6c68b52a08ac58efe2daaea2
Karl Hammar <karl@karlhammar.com>
2019-05-22 11:13:28 +0200

    Update runMac.sh

--- runMac.sh
+++ runMac.sh
@@ -1,5 +1,5 @@
 PROTEGE_PATH=/Applications/Protégé.app
-PROTEGE_PLUGINS_PATH=$PROTEGE_PATH/Contents/Java/plugins/
+PROTEGE_PLUGINS_PATH=$PROTEGE_PATH/Contents/Java/plugins
 mvn clean install
 rm $PROTEGE_PLUGINS_PATH/CoModIDE-1.0.0-SNAPSHOT.jar
 cp ./target/CoModIDE-1.0.0-SNAPSHOT.jar $PROTEGE_PLUGINS_PATH/
