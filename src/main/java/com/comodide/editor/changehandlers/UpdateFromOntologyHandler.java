8789c3c99cc3aeb600155dcdee43147767e09daf
Karl Hammar <karl@karlhammar.com>
2019-09-25 21:30:19 +0200

    First attempt at reasonable edge detection

--- src/main/java/com/comodide/editor/changehandlers/UpdateFromOntologyHandler.java
+++ src/main/java/com/comodide/editor/changehandlers/UpdateFromOntologyHandler.java
@@ -1,10 +1,12 @@
 package com.comodide.editor.changehandlers;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.apache.commons.lang3.tuple.Pair;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.ClassExpressionType;
 import org.semanticweb.owlapi.model.HasProperty;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
@@ -28,8 +30,6 @@ import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.semanticweb.owlapi.model.OWLProperty;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.model.OWLUnaryPropertyAxiom;
-import org.semanticweb.owlapi.util.OWLAxiomVisitorAdapter;
-import org.semanticweb.owlapi.util.OWLClassExpressionVisitorAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -127,6 +127,7 @@ public class UpdateFromOntologyHandler
 	}
 
 	public void handleRemoveAxiom(OWLAxiom axiom, OWLOntology ontology) {
+		log.warn("[CoModIDE:UFOH] I am being told to remove: " + axiom.getAxiomWithoutAnnotations().toString());
 		if (axiom.isOfType(AxiomType.DECLARATION))
 		{
 			OWLDeclarationAxiom declarationAxiom = (OWLDeclarationAxiom) axiom;
@@ -140,13 +141,15 @@ public class UpdateFromOntologyHandler
 		}
 		else if (axiom.isOfType(AxiomType.OBJECT_PROPERTY_DOMAIN, AxiomType.OBJECT_PROPERTY_RANGE, AxiomType.DATA_PROPERTY_DOMAIN, AxiomType.DATA_PROPERTY_RANGE))
 		{
+			log.warn("[CoModIDE:UFOH] Removing axiom "  + axiom.getAxiomWithoutAnnotations().toString());
 			// Unpack the property concerned
 			@SuppressWarnings("unchecked")
 			OWLUnaryPropertyAxiom<OWLProperty> propertyAxiom = (OWLUnaryPropertyAxiom<OWLProperty>)axiom;
 			OWLProperty property = propertyAxiom.getProperty();
 			
-			// Check whether there is enough support for rendering an edge; otherwise remove
-			if (!rendersEdgeOnSchema(property, ontology)) {
+			// Check whether there is at least one domain and one range; if not, remove
+			Pair<Set<OWLEntity>,Set<OWLEntity>> domainsAndRanges = getDomainsAndRanges(property, ontology);
+			if (domainsAndRanges.getLeft().size() < 1 || domainsAndRanges.getRight().size() < 1) {
 				schemaDiagram.removeOwlEntity(property);
 			}
 		}
@@ -159,89 +162,96 @@ public class UpdateFromOntologyHandler
 	 * @param ontology
 	 * @return
 	 */
-	private boolean rendersEdgeOnSchema(OWLProperty property, OWLOntology ontology) {
+	private Pair<Set<OWLEntity>,Set<OWLEntity>> getDomainsAndRanges(OWLProperty property, OWLOntology ontology) {
 		
 		// Check how many domains and ranges are in the ontology; only render if both are 1
-		int domainCount = 0;
-		int rangeCount = 0;
+		Set<OWLEntity> domains = new HashSet<OWLEntity>();
+		Set<OWLEntity> ranges = new HashSet<OWLEntity>();
 		if (property instanceof OWLObjectProperty) {
 			OWLObjectProperty objectProperty = (OWLObjectProperty)property;
-			domainCount = ontology.getObjectPropertyDomainAxioms(objectProperty).size();
-			rangeCount = ontology.getObjectPropertyRangeAxioms(objectProperty).size();
-			
+			for (OWLObjectPropertyDomainAxiom domainAxiom: ontology.getObjectPropertyDomainAxioms(objectProperty)) {
+				if (domainAxiom.getDomain().isNamed()) {
+					domains.add(domainAxiom.getDomain().asOWLClass());
+				}
+			}
+			for (OWLObjectPropertyRangeAxiom rangeAxiom: ontology.getObjectPropertyRangeAxioms(objectProperty)) {
+				if (rangeAxiom.getRange().isNamed()) {
+					ranges.add(rangeAxiom.getRange().asOWLClass());
+				}
+			}
 		}
 		else if (property instanceof OWLDataProperty) {
 			OWLDataProperty dataProperty = (OWLDataProperty)property;
-			domainCount = ontology.getDataPropertyDomainAxioms(dataProperty).size();
-			rangeCount = ontology.getDataPropertyRangeAxioms(dataProperty).size();
-		}
-		if (domainCount == 1 && rangeCount == 1) {
-			return true;
+			for (OWLDataPropertyDomainAxiom domainAxiom: ontology.getDataPropertyDomainAxioms(dataProperty)) {
+				if (domainAxiom.getDomain().isNamed()) {
+					domains.add(domainAxiom.getDomain().asOWLClass());
+				}
+			}
+			for (OWLDataPropertyRangeAxiom rangeAxiom: ontology.getDataPropertyRangeAxioms(dataProperty)) {
+				if (rangeAxiom.getRange().isNamed()) {
+					ranges.add(rangeAxiom.getRange().asOWLDatatype());
+				}
+			}
 		}
 		
 		// Walk through the ontology, looking for scoped domains/ranges
-		boolean hasScopedDomain = false;
-		boolean hasScopedRange = false;
-		for( OWLAxiom axiom : ontology.getAxioms()) {
-			axiom.accept(new OWLAxiomVisitorAdapter() {
-				public void visit( OWLSubClassOfAxiom subClassAxiom ) {
-					// If the subclass is named and superclass is an expression, visit and figure out if a scoped range exists
-					if (subClassAxiom.getSubClass().isNamed() && subClassAxiom.getSuperClass().isAnonymous()) {
-						subClassAxiom.getSuperClass().accept(new OWLClassExpressionVisitorAdapter() {
-							public void visit (OWLObjectAllValuesFrom restriction) {
-								if (restriction.getProperty().isNamed()) {
-									// TODO: Verify that the equals() below actually works w/ objects w/ different in-memory identity
-									if (restriction.getProperty().asOWLObjectProperty().equals(property) && restriction.getFiller().isNamed()) {
-										hasScopedRange = true;
-										// TODO: How to report back here??
-									}
-								}
+		for( OWLAxiom axiom : ontology.getAxioms(AxiomType.SUBCLASS_OF)) {
+			OWLSubClassOfAxiom subClassAxiom = (OWLSubClassOfAxiom)axiom;
+			// If the subclass is named and superclass is an expression, step into and figure out if a scoped range exists
+			if (subClassAxiom.getSubClass().isNamed() && subClassAxiom.getSuperClass().isAnonymous()) {
+				if (property instanceof OWLObjectProperty) {
+					if (subClassAxiom.getSuperClass().getClassExpressionType() == ClassExpressionType.OBJECT_ALL_VALUES_FROM) {
+						OWLObjectAllValuesFrom restriction = (OWLObjectAllValuesFrom)subClassAxiom.getSuperClass();
+						if (restriction.getProperty().isNamed()) {
+							if (restriction.getProperty().asOWLObjectProperty().equals((OWLObjectProperty)property) && restriction.getFiller().isNamed()) {
+								domains.add(subClassAxiom.getSubClass().asOWLClass());
+								ranges.add(restriction.getFiller().asOWLClass());
 							}
-							public void visit (OWLDataAllValuesFrom restriction) {
-								if (restriction.getProperty().isNamed()) {
-									// TODO: Verify that the equals() below actually works w/ objects w/ different in-memory identity
-									if (restriction.getProperty().asOWLDataProperty().equals(property) && restriction.getFiller().isNamed()) {
-										// TODO: How to report back here??
-										hasScopedRange = true;
-									}
-								}
-							}
-						});
-					}
-					// If the superclass is named and the subclass is anonymous, then this is a GCI; visit and figure out if 
-					// a scoped domain exists.
-					if (subClassAxiom.getSuperClass().isNamed() && subClassAxiom.getSubClass().isAnonymous()) {
-						subClassAxiom.getSubClass().accept(new OWLClassExpressionVisitorAdapter() {
-							public void visit (OWLObjectSomeValuesFrom restriction) {
-								if (restriction.getProperty().isNamed()) {
-									// TODO: Verify that the equals() below actually works w/ objects w/ different in-memory identity
-									if (restriction.getProperty().asOWLObjectProperty().equals(property) && restriction.getFiller().isNamed()) {
-										// TODO: How to report back here??
-										hasScopedDomain = true;
-									}
-								}
-							}
-							public void visit (OWLDataSomeValuesFrom restriction) {
-								if (restriction.getProperty().isNamed()) {
-									// TODO: Verify that the equals() below actually works w/ objects w/ different in-memory identity
-									if (restriction.getProperty().asOWLDataProperty().equals(property) && restriction.getFiller().isNamed()) {
-										// TODO: How to report back here??
-										hasScopedDomain = true;
-									}
-								}
-							}
-						});
+						}
 					}
 				}
-			});
-		}
-		// If both a scoped domain and scoped range have been found, render.
-		if (hasScopedDomain && hasScopedRange) {
-			return true;
+				else if (property instanceof OWLDataProperty) {
+					if (subClassAxiom.getSuperClass().getClassExpressionType() == ClassExpressionType.DATA_ALL_VALUES_FROM) {
+						OWLDataAllValuesFrom restriction = (OWLDataAllValuesFrom)subClassAxiom.getSuperClass();
+						if (restriction.getProperty().isNamed()) {
+							if (restriction.getProperty().asOWLDataProperty().equals((OWLDataProperty)property) && restriction.getFiller().isNamed()) {
+								domains.add(subClassAxiom.getSubClass().asOWLClass());
+								ranges.add(restriction.getFiller().asOWLDatatype());
+							}
+						}
+					}
+				}
+			}
+			// If the superclass is named and the subclass is anonymous, then this is a GCI; step into and figure out if 
+			// a scoped domain exists.
+			if (subClassAxiom.getSuperClass().isNamed() && subClassAxiom.getSubClass().isAnonymous()) {
+				if (property instanceof OWLObjectProperty) {
+					if (subClassAxiom.getSubClass().getClassExpressionType() == ClassExpressionType.OBJECT_SOME_VALUES_FROM) {
+						OWLObjectSomeValuesFrom restriction = (OWLObjectSomeValuesFrom)subClassAxiom.getSubClass();
+						if (restriction.getProperty().isNamed()) {
+							if (restriction.getProperty().asOWLObjectProperty().equals((OWLObjectProperty)property) && restriction.getFiller().isNamed()) {
+								domains.add(subClassAxiom.getSuperClass().asOWLClass());
+								ranges.add(restriction.getFiller().asOWLClass());
+							}
+						}
+					}
+				}
+				else if (property instanceof OWLDataProperty) {
+					if (subClassAxiom.getSubClass().getClassExpressionType() == ClassExpressionType.DATA_SOME_VALUES_FROM) {
+						OWLDataSomeValuesFrom restriction = (OWLDataSomeValuesFrom)subClassAxiom.getSubClass();
+						if (restriction.getProperty().isNamed()) {
+							if (restriction.getProperty().asOWLDataProperty().equals((OWLDataProperty)property) && restriction.getFiller().isDatatype()) {
+								domains.add(subClassAxiom.getSuperClass().asOWLClass());
+								ranges.add(restriction.getFiller().asOWLDatatype());
+							}
+						}
+					}
+				}
+			}
 		}
 		
 		// By default we don't render anything
-		return false;
+		return Pair.of(domains, ranges);
 	}
 
 	private void handleClass(OWLOntology ontology, OWLAxiom axiom)
