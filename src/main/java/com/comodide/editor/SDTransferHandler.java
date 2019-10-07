90390a0066a3609f8020dff437e6ff4ab849674a
Karl Hammar <karl@karlhammar.com>
2019-10-07 15:34:54 +0200

    Ensure quiet adding of metadata ontology

--- src/main/java/com/comodide/editor/SDTransferHandler.java
+++ src/main/java/com/comodide/editor/SDTransferHandler.java
@@ -2,6 +2,7 @@ package com.comodide.editor;
 
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,7 +24,10 @@ import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLImportsDeclaration;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.util.SimpleIRIMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -140,7 +144,7 @@ public class SDTransferHandler extends mxGraphTransferHandler
 					IRI activeOntologyIRI = activeOntology.getOntologyID().getOntologyIRI().orNull();
 					// The metadata ontology has the same IRI as the main ontology, but with a
 					// -metadata ending
-					IRI              metadataOntologyIRI = IRI.create(activeOntologyIRI.toString(), "-metadata");
+					IRI              metadataOntologyIRI = IRI.create(activeOntologyIRI.toString(), "-comodide-metadata");
 					Set<OWLOntology> allOntologies       = modelManager.getOntologies();
 
 					// Iterate over all loaded ontologies, to find the metadata ontology if it
@@ -148,32 +152,46 @@ public class SDTransferHandler extends mxGraphTransferHandler
 					OWLOntology metadataOntology = null;
 					for (OWLOntology ont : allOntologies)
 					{
-						if (ont.getOntologyID().getOntologyIRI().orNull().equals(metadataOntologyIRI))
-						{
-							metadataOntology = ont;
-							break;
+						if (ont.getOntologyID().getOntologyIRI().isPresent()) {
+							String ontIri = ont.getOntologyID().getOntologyIRI().get().toString();
+							if (ontIri.endsWith("-comodide-metadata")) {
+								metadataOntology = ont;
+								break;
+							}
 						}
 					}
 
 					// If the metadata ontology was not found
 					if (metadataOntology == null)
 					{
-						// Get and copy existing storage path on disk, injecting "-metadata".
+						// Get and copy existing storage path on disk, injecting "-comodide-metadata".
 						URI activeOntologyPhysicalURI = modelManager.getOntologyPhysicalURI(activeOntology);
 
 						String scheme    = activeOntologyPhysicalURI.getScheme();
 						String authority = activeOntologyPhysicalURI.getAuthority();
 						String path      = activeOntologyPhysicalURI.getPath();
 
-						// Fallback solution if no file ending exists
-						String newPath = path + "-metadata.rdf";
-
-						// If there is a file ending, inject the -metadata
-						if (path.lastIndexOf(".") != -1)
-						{
-							String pathEnding = path.substring(path.lastIndexOf("."));
-							newPath = path.replace(pathEnding, ("-metadata" + pathEnding));
+						// If there is a file ending, inject the -comodide-metadata
+						String fileComponent = path;
+						String directoryComponent = "";
+						// If the path contains slashes, only treat the last component
+						// (to avoid false positives due to dots in directory names)
+						if (path.lastIndexOf("/") != -1) {
+							int lastSlash = path.lastIndexOf("/");
+							fileComponent = path.substring(lastSlash);
+							directoryComponent = path.substring(0, lastSlash);
 						}
+						if (fileComponent.lastIndexOf(".") != -1)
+						{
+							// If file ending exists, inject before it
+							String fileEnding = fileComponent.substring(fileComponent.lastIndexOf("."));
+							fileComponent = fileComponent.replace(fileEnding, ("-comodide-metadata" + fileEnding));
+						}
+						else {
+							// Fallback solution if no file ending exists
+							fileComponent = fileComponent + "-comodide-metadata.rdf";
+						}
+						String newPath = directoryComponent + fileComponent;
 
 						URI metadataOntologyPhysicalURI = new URI(
 								String.format("%s://%s%s", scheme, authority, newPath));
@@ -182,7 +200,7 @@ public class SDTransferHandler extends mxGraphTransferHandler
 						Optional<IRI> optionalMetadataOntologyVersionIri = Optional.absent();
 						OWLOntologyID metadataOid                        = new OWLOntologyID(
 								optionalMetadataOntologyIri, optionalMetadataOntologyVersionIri);
-						metadataOntology = modelManager.createNewOntology(metadataOid, metadataOntologyPhysicalURI);
+						metadataOntology = createNewOntology(metadataOid, metadataOntologyPhysicalURI, modelManager, activeOntology.getOWLOntologyManager());
 
 						// Add import of active ontology to metadata ontology
 						OWLDataFactory        factory    = metadataOntology.getOWLOntologyManager().getOWLDataFactory();
@@ -245,6 +263,46 @@ public class SDTransferHandler extends mxGraphTransferHandler
 		}
 
 		return result;
+	}
+
+	/***
+	 * This method is a copy of createNewOntology in OWLModelManager that removes the 
+	 * call to setActiveOntology() and the firing of the ONTOLOGY_CREATED event, such 
+	 * that when a new metadata ontology is added by drag and dropping a pattern 
+	 * with ComodideConfiguration.getModuleMetadataExternal() set to true, 
+	 * that new metadata ontology is just added silently, in the background.
+	 * 
+	 * To the best of my knowledge no important functionality depends on the 
+	 * ONTOLOGY_CREATED event: OWLWorkspace activates tab 0 when catching it, and
+	 * OWLOntologyHierarchyProvider (an internal component in OntologyImportsWalker, 
+	 * instances of which would typically be transient anyway) uses it to rebuild the
+	 * imports hierarchy. Neither of these features matter to CoModIDE.
+	 * 
+	 * @param ontologyID
+	 * @param physicalURI
+	 * @param modelManager
+	 * @param manager
+	 * @return
+	 * @throws OWLOntologyCreationException
+	 */
+	private OWLOntology createNewOntology(OWLOntologyID ontologyID, URI physicalURI, OWLModelManager modelManager, OWLOntologyManager manager)
+			throws OWLOntologyCreationException {
+		if (physicalURI != null && ontologyID.getDefaultDocumentIRI().isPresent()) {
+			manager.getIRIMappers()
+					.add(new SimpleIRIMapper(ontologyID.getDefaultDocumentIRI().get(), IRI.create(physicalURI)));
+		}
+		OWLOntology ont = manager.createOntology(ontologyID);
+		if (physicalURI != null) {
+			try {
+				File containingDirectory = new File(physicalURI).getParentFile();
+				if (containingDirectory.exists()) {
+					modelManager.getOntologyCatalogManager().addFolder(containingDirectory);
+				}
+			} catch (IllegalArgumentException iae) {
+				log.info("Cannot generate ontology catalog for ontology at {}. {}", physicalURI, iae.getMessage());
+			}
+		}
+		return ont;
 	}
 
 	private Pair<Double, Double> getScaledDropLocation(mxGraphComponent graphComponent)
