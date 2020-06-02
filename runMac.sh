02de8d65048a63955a2b0e34f49fb0561ee6ff44
Karl Hammar <karl@karlhammar.com>
2020-06-02 08:19:50 +0200

    Updated version numbers for 1.1.1 release.

--- runMac.sh
+++ runMac.sh
@@ -1,6 +1,6 @@
 PROTEGE_PATH=/Applications/Protégé.app
 PROTEGE_PLUGINS_PATH=$PROTEGE_PATH/Contents/Java/plugins
 mvn clean install
-rm $PROTEGE_PLUGINS_PATH/CoModIDE-1.1.0.jar
-cp ./target/CoModIDE-1.1.0.jar $PROTEGE_PLUGINS_PATH/
+rm $PROTEGE_PLUGINS_PATH/CoModIDE-1.1.1.jar
+cp ./target/CoModIDE-1.1.1.jar $PROTEGE_PLUGINS_PATH/
 open $PROTEGE_PATH
--- runWindows.bat
+++ runWindows.bat
@@ -1,5 +1,5 @@
 set PROTEGE_PATH="C:\Program Files\Protege-5.5.0\run.bat"
 set PROTEGE_PLUGINS_PATH="C:\Program Files\Protege-5.5.0\plugins\"
-del %PROTEGE_PLUGINS_PATH%CoModIDE-1.1.0.jar
-copy target\CoModIDE-1.1.0.jar %PROTEGE_PLUGINS_PATH%
+del %PROTEGE_PLUGINS_PATH%CoModIDE-1.1.1.jar
+copy target\CoModIDE-1.1.1.jar %PROTEGE_PLUGINS_PATH%
 %PROTEGE_PATH%
