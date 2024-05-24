969de7691937828c97afd579a40b3d6a819b01c5
jonispatented <rockerjon01@gmail.com>
2024-05-23 21:47:34 -0400

    Refactored the parseIndex method of PatternLibrary.java in an effort to understand how it works. Lots was learned.

--- src/main/java/com/comodide/patterns/PatternLibrary.java
+++ src/main/java/com/comodide/patterns/PatternLibrary.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
@@ -38,29 +39,29 @@ public class PatternLibrary {
 
 	// Constants
 	public static final String DEFAULT_LIBRARY_PATH = "modl/default/ModlIndex.owl";
-	
+
 	// Infrastructure
     private static PatternLibrary instance;
     private static final Logger log = LoggerFactory.getLogger(PatternLibrary.class);
-    
+
     // Configuration fields
-    private final IRI PATTERN_CLASS_IRI = IRI.create("http://ontologydesignpatterns.org/opla#Pattern"); 
+    private final IRI PATTERN_CLASS_IRI = IRI.create("http://ontologydesignpatterns.org/opla#Pattern");
     private final IRI CATEGORIZATION_PROPERTY_IRI = IRI.create("http://ontologydesignpatterns.org/opla#categorization");
     private final IRI SCHEMADIAGRAM_PROPERTY_IRI = IRI.create("http://ontologydesignpatterns.org/opla#renderedSchemaDiagram");
     private final IRI HTMLDOC_PROPERTY_IRI = IRI.create("http://ontologydesignpatterns.org/opla#htmlDocumentation");
     private final IRI OWLREP_PROPERTY_IRI = IRI.create("http://ontologydesignpatterns.org/opla#owlRepresentation");
     public final PatternCategory ANY_CATEGORY = new PatternCategory("Any", IRI.create("https://w3id.org/comodide/ModlIndex#AnyCategory"));
-    
+
     // Instance fields
 	private OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 	private OWLDataFactory factory = manager.getOWLDataFactory();
 	private OWLOntology index;
-	private Map<PatternCategory,List<Pattern>> patternCategories = new HashMap<PatternCategory,List<Pattern>>();
-    
+	private Map<PatternCategory,List<Pattern>> patternCategories = new HashMap<>();
+
     // Singleton access method
     public static synchronized PatternLibrary getInstance() {
         if(instance == null){
-			setInstanceByFilePath("modl/default/ModlIndex.owl");
+			setInstanceByFilePath(DEFAULT_LIBRARY_PATH);
         }
         return instance;
     }
@@ -68,12 +69,12 @@ public class PatternLibrary {
 	public static synchronized void setInstanceByFilePath(@Nonnull final String filePath) {
 		instance = new PatternLibrary(filePath);
 	}
-	
+
     // Parse index on instance creation
 	private PatternLibrary(@Nonnull final String filePath) {
 		parseIndex(filePath);
 	}
-	
+
 	// Based on pattern object, instantiate pattern from disk into OWLOntology
 	public OWLOntology getOwlRepresentation(Pattern pattern) throws OWLOntologyCreationException {
 		// TODO: add support for an external pattern library
@@ -85,7 +86,7 @@ public class PatternLibrary {
 		}
 		return exportManager.loadOntologyFromOntologyDocument(is);
 	}
-	
+
 	/**
 	 * Parses a pattern index and updates the data structures that are needed to feed other classes.
 	 */
@@ -95,91 +96,78 @@ public class PatternLibrary {
 			ClassLoader classloader = this.getClass().getClassLoader();
 			InputStream is = classloader.getResourceAsStream(filePath);
 			index = manager.loadOntologyFromOntologyDocument(is);
-			
+
 			// Regardless of index structure there is always the Any category
-			patternCategories.put(ANY_CATEGORY, new ArrayList<Pattern>());
-			
+			patternCategories.put(ANY_CATEGORY, new ArrayList<>());
+
 			// Find all the pattern instances in the index
 			OWLClass patternClass = factory.getOWLClass(PATTERN_CLASS_IRI);
 			OWLObjectProperty categorizationProperty = factory.getOWLObjectProperty(CATEGORIZATION_PROPERTY_IRI);
 			OWLDataProperty schemaDiagram = factory.getOWLDataProperty(SCHEMADIAGRAM_PROPERTY_IRI);
 			OWLDataProperty htmlDocumentation = factory.getOWLDataProperty(HTMLDOC_PROPERTY_IRI);
 			OWLDataProperty owlRepresentationProperty = factory.getOWLDataProperty(OWLREP_PROPERTY_IRI);
-			for (OWLIndividual pattern: EntitySearcher.getIndividuals(patternClass, index)) {
-				if (pattern.isNamed()) {
-					
-					// We've found a pattern; turn it into a Java object.
-					OWLNamedIndividual namedPattern = (OWLNamedIndividual)pattern;
-					List<String> patternLabels = getLabels(namedPattern, index);
-					List<String> owlRepresentations = getDataPropertyValues(namedPattern, owlRepresentationProperty, index);
-					
-					// Mandatory fields if we are to proceed at all: rdfs:label, opla:owlRepresentation, 
-					// and an IRI (which is already checked above in if pattern.isNamed())
-					if (patternLabels.size() > 0 && owlRepresentations.size() > 0) {
+
+			EntitySearcher.getIndividuals(patternClass, index).stream()
+					.filter(OWLIndividual::isNamed)
+					.map(individual -> (OWLNamedIndividual) individual)
+					.forEach(namedPattern -> {
+						// We've found a pattern; turn it into a Java object.
+						List<String> patternLabels = getLabels(namedPattern, index);
+						List<String> owlRepresentations = getDataPropertyValues(namedPattern, owlRepresentationProperty, index);
+
+						// Mandatory fields if we are to proceed at all: rdfs:label, opla:owlRepresentation,
+						// and an IRI (which is already checked above in if pattern.isNamed())
+						if (patternLabels.isEmpty() || owlRepresentations.isEmpty())
+							return;
+
 						Pattern newPattern = new Pattern(patternLabels.get(0), namedPattern.getIRI(), owlRepresentations.get(0));
-					
+
 						// The below fields are nice to have but not mandatory to be indexed.
-						List<String> schemaDiagrams = getDataPropertyValues(namedPattern, schemaDiagram, index);
-						if (schemaDiagrams.size() > 0) {
-							newPattern.setSchemaDiagramPath(schemaDiagrams.get(0));
-						}
-						List<String> htmlDocs = getDataPropertyValues(namedPattern, htmlDocumentation, index);
-						if (htmlDocs.size() > 0) {
-							newPattern.setHtmlDocumentation(htmlDocs.get(0));
-						}
-						
+						getDataPropertyValues(namedPattern, schemaDiagram, index).stream()
+								.findFirst().ifPresent(newPattern::setSchemaDiagramPath);
+						getDataPropertyValues(namedPattern, htmlDocumentation, index).stream()
+								.findFirst().ifPresent(newPattern::setHtmlDocumentation);
+
 						// Find all the categories for this pattern. All patterns are assigned to at least the Any category by default
-						List<PatternCategory> categoriesForPattern = new ArrayList<PatternCategory>();
-						categoriesForPattern.add(ANY_CATEGORY);
-						for (OWLIndividual category: EntitySearcher.getObjectPropertyValues(namedPattern, categorizationProperty, index)) {
-							if (category.isNamed()) {
-								
-								// We've found a category; turn it into a Java object and and add to list
-								OWLNamedIndividual namedCategory = (OWLNamedIndividual)category;
-								List<String> categoryLabels = getLabels(namedCategory, index);
-								String categoryLabel;
-								if (categoryLabels.size() > 0) {
-									categoryLabel = categoryLabels.get(0);
-								}
-								else {
-									categoryLabel = namedCategory.getIRI().toString();
-								}
-								PatternCategory newCategory = new PatternCategory(categoryLabel, namedCategory.getIRI());
-								categoriesForPattern.add(newCategory);
-							}
-						}
-						
-						// Go through the list of categories for pattern and generate the map structure Category -> List<Pattern> that we need 
-						for (PatternCategory category: categoriesForPattern) {
-							if (patternCategories.containsKey(category)) {
-								List<Pattern> patternsForCategory = patternCategories.get(category);
-								patternsForCategory.add(newPattern);
-							}
-							else {
-								List<Pattern> patternsForCategory = new ArrayList<Pattern>();
-								patternsForCategory.add(newPattern);
-								patternCategories.put(category, patternsForCategory);
-							}
-						}
-					}
-				}
-			}
-		} 
+						List<PatternCategory> categoriesForPattern =
+								EntitySearcher.getObjectPropertyValues(namedPattern, categorizationProperty, index).stream()
+								.filter(OWLIndividual::isNamed)
+								.map(individual -> (OWLNamedIndividual) individual)
+								.map(namedCategory -> {
+									List<String> categoryLabels = getLabels(namedCategory, index);
+									String categoryLabel = !categoryLabels.isEmpty() ?
+											categoryLabels.get(0) :
+											namedCategory.getIRI().toString();
+									return new PatternCategory(categoryLabel, namedCategory.getIRI());
+								})
+								.collect(Collectors.toList());
+						categoriesForPattern.add(0, ANY_CATEGORY);
+
+						// Go through the list of categories for pattern and generate the map structure Category -> List<Pattern> that we need
+						categoriesForPattern.forEach(category -> {
+							patternCategories.putIfAbsent(category, new ArrayList<>());
+							patternCategories.computeIfPresent(category, (patternCategory, patterns) -> {
+                                patterns.add(newPattern);
+								return patterns;
+							});
+						});
+					});
+		}
 		catch (Exception e) {
 			log.error("Unable to reindex pattern library; setting up empty index.", e);
 			e.printStackTrace();
 		}
     }
-    
+
 	/**
 	 * Convenience method that returns the string values of all rdfs:label annotations on
 	 * a given entity in a given ontology.
-	 * @param entity 
+	 * @param entity
 	 * @param ontology
 	 * @return
 	 */
 	private List<String> getLabels(OWLEntity entity, OWLOntology ontology) {
-		List<String> retVal = new ArrayList<String>();
+		List<String> retVal = new ArrayList<>();
 		for(OWLAnnotation annotation: EntitySearcher.getAnnotations(entity, ontology, factory.getRDFSLabel())) {
 		    OWLAnnotationValue value = annotation.getValue();
 		    if(value instanceof OWLLiteral) {
@@ -188,25 +176,25 @@ public class PatternLibrary {
 		}
 		return retVal;
 	}
-	
+
 	/**
-	 * Convenience method that returns the string values of all occurrences of a given data 
-	 * property on a given individual in a given ontology 
+	 * Convenience method that returns the string values of all occurrences of a given data
+	 * property on a given individual in a given ontology
 	 * @param i - Individual
 	 * @param p - Data property
 	 * @param ontology - Ontology
 	 * @return List of strings, one per each occurrence of the data property, on the individual, in the ontology
 	 */
 	private List<String> getDataPropertyValues(OWLIndividual i, OWLDataProperty p, OWLOntology ontology) {
-		List<String> retVal = new ArrayList<String>();
+		List<String> retVal = new ArrayList<>();
 		for(OWLLiteral literal: EntitySearcher.getDataPropertyValues(i, p, ontology)) {
 			retVal.add(literal.getLiteral());
 		}
 		return retVal;
-		
-		
+
+
 	}
-	
+
 	/**
 	 * Returns a list of all categories (that have one or more patterns) in this library.
 	 * @return
@@ -214,15 +202,15 @@ public class PatternLibrary {
 	public PatternCategory[] getPatternCategories() {
 		// Get and sort all categories
 		Set<PatternCategory> categorySet = patternCategories.keySet();
-		List<PatternCategory> categoryList = new ArrayList<PatternCategory>(categorySet);
+		List<PatternCategory> categoryList = new ArrayList<>(categorySet);
 		Collections.sort(categoryList);
 		// Put the Any category first in the list, for usability purposes
 		categoryList.remove(ANY_CATEGORY);
 		categoryList.add(0, ANY_CATEGORY);
 		// Return as array
-		return categoryList.toArray(new PatternCategory[categoryList.size()]);
+		return categoryList.toArray(new PatternCategory[0]);
 	}
-	
+
 	/**
 	 * Returns all the patterns for a given category.
 	 * @param category
