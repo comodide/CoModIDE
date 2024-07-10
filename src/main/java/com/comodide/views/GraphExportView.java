43829535295f627211769363cca476d685863721
Ambar-Shrivastava <amber.shrivastava27@outlook.com>
2024-07-10 13:21:31 -0400

    EdgeInspectorView changed for diplaying edge label

--- src/main/java/com/comodide/views/GraphExportView.java
+++ src/main/java/com/comodide/views/GraphExportView.java
@@ -17,8 +17,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
 
-import org.apache.pdfbox.pdmodel.*;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 
 public class GraphExportView extends AbstractOWLViewComponent
@@ -37,30 +35,6 @@ public class GraphExportView extends AbstractOWLViewComponent
 
             File fileName = new File(outputFilepath);
             ImageIO.write(image, fileType, fileName);
-
-    }
-    //method to export graph in pdf format
-    public static void exportToPdf(String outputFilepath, mxGraphComponent graphComponent) throws IOException
-    {
-
-        BufferedImage bufferedImage = mxCellRenderer.createBufferedImage
-                (graphComponent.getGraph(), null, 1, Color.WHITE, graphComponent.isAntiAlias(), null, graphComponent.getCanvas());
-
-        String tempFilepath="temp.png";
-        File fileName = new File(tempFilepath);
-        ImageIO.write(bufferedImage, "PNG", fileName);
-
-        PDDocument pdDocument = new PDDocument();
-        PDPage pdPage = new PDPage();
-        pdDocument.addPage(pdPage);
-        PDImageXObject pdImage = PDImageXObject.createFromFile(tempFilepath, pdDocument);
-
-        PDPageContentStream pdPageContentStream = new PDPageContentStream(pdDocument, pdPage);
-        pdPageContentStream.drawImage(pdImage, 50, 50);
-        pdPageContentStream.close();
-
-        pdDocument.save(outputFilepath);
-        pdDocument.close();
 
     }
 
@@ -116,32 +90,6 @@ public class GraphExportView extends AbstractOWLViewComponent
         }
     };
 
-    //adding action listener for pdf button
-    private  final ActionListener pdfButtonsListener = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser jfc = new JFileChooser();
-            jfc.setDialogTitle("Save as pdf");
-            int userSelection = jfc.showSaveDialog(graphComponent);
-
-            if (userSelection == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = jfc.getSelectedFile();
-                String selectedFileAbsolutePath = (selectedFile.getAbsolutePath()).toLowerCase();
-
-                if (!selectedFileAbsolutePath.endsWith(".pdf")) {
-                    selectedFileAbsolutePath += "pdf";
-                }
-                try {
-                    exportToPdf(selectedFileAbsolutePath, graphComponent);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-                JOptionPane.showMessageDialog(null, "Graph Exported Successfully", "Export Status", JOptionPane.INFORMATION_MESSAGE);
-            }
-        }
-    };
-
-
 
     @Override
     protected void initialiseOWLView() throws Exception {
@@ -153,11 +101,10 @@ public class GraphExportView extends AbstractOWLViewComponent
 
         JButton jpgExportButton = new JButton("JPG");
         JButton pngExportButton = new JButton("PNG");
-        JButton pdfExportButton = new JButton("PDF");
+        JButton pdfExportButton = new JButton("PDF Button Under Construction");
 
         jpgExportButton.addActionListener(jpgButtonsListener);
         pngExportButton.addActionListener(pngButtonsListener);
-        pdfExportButton.addActionListener(pdfButtonsListener);
 
         this.add(jpgExportButton);
         this.add(pngExportButton);
